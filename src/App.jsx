06603bd3c760730b3805deb00b5121efd00d9aaf
Andrew Cao <andrewcao618@gmail.com>
2025-02-27 22:22:03 -0500

    Added Characters for Memory Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const cards = [
+    { name: "V", img: "./images/v-cyberpunk.jpg" },
+    {
+      name: "Jackie Welles",
+      img: "./images/jackie-welles.jpg",
+    },
+    { name: "Johnny Silverhand", img: "./images/johnny-silverhand.jpg" },
+    { name: "Judy Alvarez", img: "./images/judy.jpg" },
+    { name: "Misty", img: "./images/misty.jpg" },
+    { name: "Panam Palmer", img: "./images/panam-palmer.jpg" },
+    { name: "Solomon Reed", img: "./images/solomon-reed.jpg" },
+    { name: "Songbird", img: "./images/songbird.jpg" },
+    { name: "Viktor Vektor", img: "./images/viktor-vektor.jpg" },
+    { name: "Dexter DeShawn", img: "./images/dexter-deshawn.jpg" },
+  ];
 }
 
-export default App
+export default App;
